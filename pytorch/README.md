5d57ce1d6c4a92b179f00295d0d3d7bfdd99138c
matde-je <matidlecosta719@gmail.com>
2025-02-10 10:36:33 +0000

    updated

--- pytorch/README.md
+++ pytorch/README.md
@@ -1,9 +1,32 @@
-# Compiling
+# Running
 
-To run this code, you need to install all dependencies such as torch, torchvision and pillow (PIL). We advise you to also create and activate a virtual python environment to install these libraries and to run the code.
+To run this code, you need to install all dependencies such as torch, torchvision and pillow (PIL). I advise you to create and activate a virtual python environment to install these libraries and run the code.
 
-To start training the model, run the following inside the pytorch directory:
+# Training
+To start training the model, run the following, inside the pytorch directory:
 
 ```bash
 python train.py
+```
+
+# Testing
+To test the model, run the following, inside the pytorch directory:
+
+```bash
+python test.py
+```
+
+# Converting
+
+To convert the pytorch model to tensorrt, you need to send to the jetson nano the model (.pth), and the convert.py script, you can do this my running:
+
+```bash
+chmod +x send_data.sh
+send_data.sh
+```
+
+After this, run in jetson the converting script, in a virtual environment, be sure to install tensorrt as well:
+
+```bash
+python convert.py
 ```
--- pytorch/model.py
+++ pytorch/model.py
@@ -8,18 +8,18 @@ from torch.utils.data import Dataset, DataLoader
 #binary classification
 class LaneNet(nn.Module): #neural network
     def __init__(self):
-        super(LaneNet, self).__init__()
+        super(LaneNet, self).__init__() #initialize and inherit the necessary functionality from parent class 
         self.encoder = nn.Sequential( # feature extraction
             nn.Conv2d(3, 64, kernel_size=3, padding=1), # Extracts features from the image by applying filters (kernels)
             nn.ReLU(), # non-linearity so the network can learn complex patterns
             nn.Conv2d(64, 128, kernel_size=3, padding=1), #Learn spatial patterns
-            nn.ReLU(),
+            nn.ReLU(), # each convolutional layer only learns linear transformations
             nn.MaxPool2d(2) #reduce size & computation by half, downsample
         )
         self.decoder = nn.Sequential( #Reconstructs the output
             nn.Conv2d(128, 64, kernel_size=3, padding=1), #refine details
             nn.ReLU(),
-            nn.Conv2d(64, 1, kernel_size=1), #Output channels = 1 (binary mask)
+            nn.Conv2d(64, 1, kernel_size=1), #binary mask
         )
 
     def forward(self, x):
