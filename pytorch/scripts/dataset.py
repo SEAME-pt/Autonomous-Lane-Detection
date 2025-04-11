e324df7cadc5c2586d63d874f049440d4f92927e
matde-je <matildecosta719@gmail.com>
2025-04-11 11:45:32 +0100

    deploy jetson

--- pytorch/scripts/dataset.py
+++ pytorch/scripts/dataset.py
@@ -9,7 +9,7 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
 train_transforms = A.Compose([
-    A.Resize(height=144, width=256),  # Ensure consistent input size 
+    A.Resize(height=256, width=256),  # Ensure consistent input size 
     A.HorizontalFlip(p=0.5),
     A.RandomBrightnessContrast(p=0.3),
     A.RandomGamma(p=0.3),
@@ -22,7 +22,7 @@ train_transforms = A.Compose([
 ])
 
 test_transforms = A.Compose([
-    A.Resize(height=144, width=256),
+    A.Resize(height=256, width=256),
     A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ToTensorV2(),
 ])
